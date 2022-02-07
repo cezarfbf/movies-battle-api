139f95f038630fea24baebad84f93b941db0025b
Cezar Filho <cezar.filho@timwe.com>
2022-02-07 19:18:31 +0000

    add swagger

---
+++ src/test/java/com/moviesbattle/moviesbattleapi/MoviesBattleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesbattle.moviesbattleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBattleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
